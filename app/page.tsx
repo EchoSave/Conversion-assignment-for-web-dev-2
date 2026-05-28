1f372c819eba3a2619c5f7d4aba91742c852010f
Daniel-Watson-Sait <daniel.watson1@edu.sait.ca>
2026-05-28 15:26:12 -0600

    Added navbar and pages for each conversion.

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="max-w-4xl mx-auto px-6 py-12 space-y-16">
+      <section id="about" className="text-center py-12">
+        <h1 className="text-4xl font-bold text-gray-900 mb-2">Unit Conversions</h1>
+
+        <p>
+            Easily convert between different units of measurement.
+            Pounds (lb.) to kilograms (kg), and vice versa
+            Miles to kilometres (km), and vice versa
+            Celsius (°C) to Fahrenheit (°F), and vice versa
+
+        </p>
+      </section>
       
     </div>
   );
